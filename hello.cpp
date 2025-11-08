5e6a80a99bcda5f2ec7859e684bd55e51e79adc8
SevaKarlov2005 <sevaneva05@gmail.com>
2025-11-09 00:21:19 +0300

    Add hello.h, hello.cpp and README.md files

---
+++ hello.cpp
@@ -0,0 +1,6 @@
+#include "hello.h"
+
+void Welcome()
+{
+    cout << "Hello, dear User!" << endl;
+}
--- main.cpp
+++ main.cpp
@@ -1,8 +1,15 @@
 #include <iostream>
+#include "hello.h"
 
 using namespace std;
 
 int main()
 {
+    int a;
+
+    Welcome();
+
+    cin >> a;
+
     return 0;
 }
